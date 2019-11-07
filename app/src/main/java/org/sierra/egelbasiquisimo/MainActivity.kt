8518d10e94c09bce54cf901766f4ab5d2ad6a258
Gera <humbertova_mx@hotmail.com>
2019-11-06 22:59:50 -0600

    asasas

--- app/src/main/java/org/sierra/egelbasiquisimo/MainActivity.kt
+++ app/src/main/java/org/sierra/egelbasiquisimo/MainActivity.kt
@@ -2,20 +2,24 @@ package org.sierra.egelbasiquisimo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_inicio.*
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.layout_inicio)
+/**
         val recucleView=  recyclerview as RecyclerView
         val adaptador=TemasListAdapter(this)
         recucleView.adapter=adaptador
@@ -25,6 +29,41 @@ class MainActivity : AppCompatActivity() {
 
 
         Toast.makeText(applicationContext,"Valor:"+chingaTuPutaCogidaMadreElQueLoLea().size, Toast.LENGTH_LONG).show()
+        **/
+
+
+val temas=ArrayList<String>()
+
+        temas.add("A1. Diagnóstico del problema")
+        temas.add("A2. Modelado de los requerimientos")
+
+        //Obtenemos del sprinner
+        var spinner=   spinner
+
+
+        var adapter= ArrayAdapter<String>(applicationContext, android.R.layout.simple_spinner_item, temas)
+
+        adapter.setDropDownViewResource(R.layout.simple_spinner)
+// Apply the adapter to the spinner
+        spinner.adapter = adapter
+
+        spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var item=        parent?.getItemAtPosition(position)
+                //El siguiente funciona bien
+                  Toast.makeText(applicationContext,"Seleccionaste este elemento "+item,Toast.LENGTH_SHORT).show()
+                // if(item=="Otros") textoOtos.visibility=View.VISIBLE
+
+
+            }
+
+        }
+
+
     }
 
 
@@ -41,13 +80,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        //  Toast.makeText(this, "Pero es Pregunta ${pregunta?.area} ", Toast.LENGTH_LONG).show()
-
-        //  var media= MediaPlayer()
-        //   var descriptor=application.assets.openFd("sad.mp3")
-        //    media.setDataSource(descriptor.fileDescriptor,descriptor.startOffset,descriptor.length)
-        //     media.prepare()
-        //     media.start()
 
         return preguntasListas!!
     }
--- app/src/main/java/org/sierra/egelbasiquisimo/TemasListAdapter.kt
+++ app/src/main/java/org/sierra/egelbasiquisimo/TemasListAdapter.kt
@@ -19,7 +19,9 @@ class TemasListAdapter internal constructor(
         val nombreItemView: TextView = itemView.findViewById(R.id.textoPregunta)
         //Aqui irian otras vistas invocadas con el view  elemal y el id
         val radio1View: RadioButton=itemView.findViewById(R.id.radioButton1)
-
+        val radio2View: RadioButton=itemView.findViewById(R.id.radioButton2)
+        val radio3View: RadioButton=itemView.findViewById(R.id.radioButton3)
+        val radio4View: RadioButton=itemView.findViewById(R.id.radioButton4)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TemasViewHolder {
@@ -31,6 +33,9 @@ class TemasListAdapter internal constructor(
         val current = temas[position]
         holder.nombreItemView.text = current.pregunta
         holder.radio1View.text=current.opciones[0].titulo
+        holder.radio2View.text=current.opciones[1].titulo
+        holder.radio3View.text=current.opciones[2].titulo
+        holder.radio4View.text=current.opciones[3].titulo
     }
 
     internal fun setTemas(temas: List<Temas>) {
